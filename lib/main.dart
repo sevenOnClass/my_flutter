a8ae33b9b77c7b307bd8ece4e5fb276224d8cfd5
庄琦 <zhuangqi@iyunxiao.com>
2019-03-07 20:53:53 +0800

    自定义菜单布局

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,96 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // 尝试直接使用row和column
 //    return Container(
 //      child: buildExpandedImages(),
 //    );
+
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(Icons.star,color: Colors.green[500]),
+        Icon(Icons.star,color: Colors.green[500]),
+        Icon(Icons.star,color: Colors.green[500]),
+        Icon(Icons.star,color: Colors.black),
+        Icon(Icons.star,color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PPEP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.timer, color: Colors.green[500]),
+                Text('Cook:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Text('FEEDS:'),
+                Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: <Widget>[
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+
+
     return MaterialApp(
       title:'Flutter layout demo',
       home: Scaffold(
@@ -20,11 +107,26 @@ class MyApp extends StatelessWidget {
           title: Text('layout demo'),
         ),
         body: Center(
-          child: buildExpandedImages(),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            height: 700,
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width:400,
+                    child: leftColumn,
+                ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
 
 
   Widget buildOverflowingRow() => Row(
