bd50eec958e71dadcd6a052f8b968635963be810
庄琦 <zhuangqi@iyunxiao.com>
2019-03-11 18:18:32 +0800

    MOD：初始化Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -7,62 +7,20 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
     return new MaterialApp(
-      title: 'Welcome to Flutte',
-//      theme: new ThemeData(
-////        primaryColor: Colors.white,
-//          primarySwatch: Colors.blue,
-//      ),
-
-//      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-//      home: new RandomWords(),
-//        home: new Center(
-//          child: Image.network(
-//            'http://jspang.com/static/myimg/blogtouxiang.jpg',
-////            color: Colors.greenAccent,
-////            colorBlendMode: BlendMode.darken,
-//          ),
-        home: Scaffold(
-          body: Center(
-            child: Container(
-              height: 200.0,
-              child: MyList(),
-            ),
-          ),
-        ),
-//      routes: {
-//        "new_page":(context)=>NewRoute(),
-//      },
+       title: 'Flutter Layout Demo',
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text('Flutter layout demo'),
+         ),
+         body: Center(
+           child: Text('Hello World'),
+         ),
+       ),
     );
+
   }
 }
 
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        new Container(
-          width: 180,
-          color: Colors.lightBlue,
-        ),
-        new Container(
-          width: 180,
-          color: Colors.amber,
-        ),new Container(
-          width: 180,
-          color: Colors.deepOrange,
-        ),new Container(
-          width: 180,
-          color: Colors.deepPurple,
-        ),
-      ],
-    );
-  }
-}
 
 
