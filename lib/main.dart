0985a0be6d12ec9bfb5ad86f649377959eb948d8
庄琦 <zhuangqi@iyunxiao.com>
2019-03-11 09:55:41 +0800

    MOD: 使用三房库

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to Flutte1',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
+          title: new Text('Welcome to Flutter2ff'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new RandomWords();
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final word = new WordPair.random();
+    return new Text(word.asPascalCase);
   }
 }
