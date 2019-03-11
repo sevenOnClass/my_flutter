4876d62815b94322bbd60bea867f0c7b867565e3
庄琦 <zhuangqi@iyunxiao.com>
2019-03-11 11:12:33 +0800

    MOD：自定义cell, leading, subtitle，trailing, onTap使用

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,7 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
       title: 'Welcome to Flutte1',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter2ff'),
-        ),
-        body: new Center(
-          child: new RandomWords();
-        ),
-      ),
+      home: new RandomWords(),
     );
   }
 }
@@ -31,10 +24,71 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    final word = new WordPair.random();
-    return new Text(word.asPascalCase);
+    // TODO: implement buildfff
+//    final word = new WordPair.random();
+//    return new Text(word.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Genrated Word'),
+      ),
+      body: _buildSetting(),
+    );
+  }
+
+  Widget _buildSetting() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i){
+        if(i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair word) {
+    final alreadySaved = _saved.contains(word);
+
+    return new ListTile(
+      leading: new CircleAvatar(
+        child: Image.asset('images/pic1.jpg'),
+        radius: 20,
+      ) ,
+      title: new Text(
+        word.asPascalCase,
+        style: _biggerFont,
+      ),
+      subtitle: new Text(
+        _suggestions.indexOf(word).toString(),
+        style: new TextStyle(
+          color: Colors.red[200],
+        ),
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(word);
+          }else {
+            _saved.add(word);
+          }
+        });
+      },
+    );
   }
 }
