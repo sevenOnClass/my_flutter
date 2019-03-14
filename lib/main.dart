b34ad0c729d056fb6b7182cb03e36534567b966c
庄琦 <zhuangqi@iyunxiao.com>
2019-03-14 15:36:24 +0800

    MOD: 更新Flutter首页

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'randomword.dart';
 import 'myhomepage.dart';
 import 'myAppDetail.dart';
 
-void main() => runApp(new MyAppDetail());
+void main() => runApp(new MainAPP());
 
-
+class MainAPP extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: 'Test0312',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: Center(
+          child: Text('hello seven'),
+        ),
+      ),
+    );
+  }
+}
 
 
